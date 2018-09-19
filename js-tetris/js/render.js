a76df2c9037178bd5ad00c7e189f1c5ba336be26
shotaro-miyazaki <39908393+shotaro-miyazaki@users.noreply.github.com>
2018-09-19 09:16:50 +0200

    Add files via upload

--- js-tetris/js/render.js
+++ js-tetris/js/render.js
@@ -1,37 +1,42 @@
-let canvas = document.getElementsByTagName('canvas')[ 0 ];
-let ctx = canvas.getContext('2d');
-let W = 300, H = 600;
-let BLOCK_W = W / COLS;
-let BLOCK_H = H / ROWS;
+/*
+ 現在の盤面の状態を描画する処理
+ */
+var canvas = document.getElementsByTagName( 'canvas' )[ 0 ];  // キャンバス
+var ctx = canvas.getContext( '2d' ); // コンテクスト
+var W = 300, H = 600;  // キャンバスのサイズ
+var BLOCK_W = W / COLS, BLOCK_H = H / ROWS;  // マスの幅を設定
 
-function drawBlock(x, y) {
-  ctx.fillRect(BLOCK_W * x, BLOCK_H * y, BLOCK_W - 1, BLOCK_H - 1);
-  ctx.strokeRect(BLOCK_W * x, BLOCK_H * y, BLOCK_W - 1, BLOCK_H - 1);
+// x, yの部分へマスを描画する処理
+function drawBlock( x, y ) {
+  ctx.fillRect( BLOCK_W * x, BLOCK_H * y, BLOCK_W - 1 , BLOCK_H - 1 );
+  ctx.strokeRect( BLOCK_W * x, BLOCK_H * y, BLOCK_W - 1 , BLOCK_H - 1 );
 }
 
-function render (){
-  ctx.clearRect(0, 0, W, H); //clear canvas
-  ctx.strokeStyle = 'black'; //color set to black
+// 盤面と操作ブロックを描画する
+function render() {
+  ctx.clearRect( 0, 0, W, H );  // 一度キャンバスを真っさらにする
+  ctx.strokeStyle = 'black';  // えんぴつの色を黒にする
 
-  //make a board
-  for (let x = 0; x < COLS; ++x){
-    for (let y = 0; y < ROWS; ++y){
-      //if the cell is not
-      if ( board [ y ][ x ] ) {
-        ctx.fillStyle = colors [board [ y ][ x ] - 1]
-        drawBlock(x, y);
+  // 盤面を描画する
+  for ( var x = 0; x < COLS; ++x ) {
+    for ( var y = 0; y < ROWS; ++y ) {
+      if ( board[ y ][ x ] ) {  // マスが空、つまり0ゃなかったら
+        ctx.fillStyle = colors[ board[ y ][ x ] - 1 ];  // マスの種類に合わせて塗りつぶす色を設定
+        drawBlock( x, y );  // マスを描画
       }
     }
   }
-  //make a block
-  for (let y = 0; y < 4; ++y) {
-    for(let x = 0; x < 4; ++x) {
-      if (current[ y ][ x ]) {
-        ctx.fillStyle = colors[current [ y ][ x ] - 1];
-        drawBlock (currentX + x, currentY + y);
+
+  // 操作ブロックを描画する
+  for ( var y = 0; y < 4; ++y ) {
+    for ( var x = 0; x < 4; ++x ) {
+      if ( current[ y ][ x ] ) {
+        ctx.fillStyle = colors[ current[ y ][ x ] - 1 ];  // マスの種類に合わせて塗りつぶす色を設定
+        drawBlock( currentX + x, currentY + y );  // マスを描画
       }
     }
   }
 }
 
-setInterval (render, 30); */
+// 30ミリ秒ごとに状態を描画する関数を呼び出す
+setInterval( render, 30 );
